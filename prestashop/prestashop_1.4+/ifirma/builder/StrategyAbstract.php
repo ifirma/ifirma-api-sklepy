1fdc32fe7ef9956417fce325a670a4ceebedc204
Bartosz Bojanowicz <bbojanowicz@power.com.pl>
2013-07-08 11:36:21 +0200

    contractor address in invoice

--- prestashop/prestashop_1.4+/ifirma/builder/StrategyAbstract.php
+++ prestashop/prestashop_1.4+/ifirma/builder/StrategyAbstract.php
@@ -85,6 +85,7 @@ abstract class StrategyAbstract {
 		$contractor->{InvoiceContractor::KEY_KOD_POCZTOWY} = $this->_address->postcode;
 		$contractor->{InvoiceContractor::KEY_MIEJSCOWOSC} = $this->_address->city;
 		$contractor->{InvoiceContractor::KEY_NIP} = (!empty($this->_address->vat_number) ? $this->_address->vat_number : null);
+		$contractor->{InvoiceContractor::KEY_ULICA} = $this->_address->address1 . ' ' . $this->_address->address2;
 		$contractor->{InvoiceContractor::KEY_TELEFON} = (
 				empty($this->_address->phone_mobile)
 				?
